2ba5505abe771d62c78d96d691349ffeecf5b397
Martin Postma <martinuspostma@gmail.com>
2026-01-15 12:08:56 +0200

    fix: Add add-form link to field mapping entity definition
    - Added 'add-form' link pointing to /admin/config/rating-scorer/field-mappings/add
    - This enables the ConfigEntityListBuilder to render the 'Add' action button
    - Ensures administrators can create new field mapping configurations

--- src/Entity/RatingScorerFieldMapping.php
+++ src/Entity/RatingScorerFieldMapping.php
@@ -38,6 +38,7 @@ use Drupal\Core\Config\Entity\ConfigEntityBase;
  *     "bayesian_threshold"
  *   },
  *   links = {
+ *     "add-form" = "/admin/config/rating-scorer/field-mappings/add",
  *     "edit-form" = "/admin/config/rating-scorer/field-mappings/{rating_scorer_field_mapping}/edit",
  *     "delete-form" = "/admin/config/rating-scorer/field-mappings/{rating_scorer_field_mapping}/delete",
  *     "collection" = "/admin/config/rating-scorer/field-mappings"
